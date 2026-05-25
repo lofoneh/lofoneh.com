096d7cdd5234b800e00a3695001bdcb85852fa4f
Jeffery Lofoneh Asamani <jefferyasamani7@gmail.com>
2026-05-25 02:09:32 +0000

    update LinkedIn URL in footer social links

--- src/navigation.ts
+++ src/navigation.ts
@@ -15,7 +15,7 @@ export const footerData = {
   socialLinks: [
     { ariaLabel: 'GitHub', icon: 'tabler:brand-github', href: 'https://github.com/lofoneh' },
     { ariaLabel: 'X (Twitter)', icon: 'tabler:brand-x', href: 'https://x.com/lofoneh' },
-    { ariaLabel: 'LinkedIn', icon: 'tabler:brand-linkedin', href: 'https://www.linkedin.com/in/lofoneh' },
+    { ariaLabel: 'LinkedIn', icon: 'tabler:brand-linkedin', href: 'https://www.linkedin.com/in/jeffery-asamani/' },
     { ariaLabel: 'Email', icon: 'tabler:mail', href: 'mailto:jefferyasamani7@gmail.com' },
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
   ],
